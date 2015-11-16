5ac7873264f7202d6f7decc599975ee9b05e78ab
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2015-11-16 15:23:14 -0600

    added the button and css

---
+++ src/button.js
@@ -0,0 +1,13 @@
+
+var num = 0;
+
+$(`button`).click(function() {
+  num++;
+  $(`button`).html(num + '' + ' Like');
+
+  if (num <= 1) {
+    return true;
+  } else {
+    return $(`button`).html(num + '' + ' Likes');
+  };
+});
